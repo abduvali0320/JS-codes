4320cb642f4858ca5b73ba96e5326a40841e4469
abduvali0320 <abduvalinormirzayev007@gmail.com>
2024-02-26 19:49:39 +0500

    git folder is delete in some folder

---
+++ typing test/js.js
@@ -0,0 +1,105 @@
+let text = "Kompaniyaning o'zi juda muvaffaqiyatli kompaniya. U mehnat ehtiyojlaridan mamnun bo'lishni xohlamaydi va shunga o'xshash narsalarning muntazamligida oson narsadan ko'ra ko'proq harakat qilish uchun xushomadgo'ylik bilan. Eng buzuq tuyg'ularning rohatini olish uchun tanlov qilish naqadar buyukdir! Qachonki odoblar ozmi-ko'pmi ko'rinsa ham, bu dardlar buzuqning vasvasasidan ham bo'lmaydi. Shunday bo'ladiki, farq yo'q qilinadi va biz mashg'ulotlarni qabul qilamiz va og'riqning o'zi tufayli hech kim katta ish qila olmaydi. Hech kim haqiqatga berilmaydi, lekin u buni eng yomon ko'radi. Va bu qarz tufayli, oqsoqollar va me'mor tanlanganni hech qachon tark etmadimi? Uni hech kim kechirmaydi, go'yo hamma qochib ketadi! U mana shu zahmatkash ehtiyojlarning bir qismi kabi qarzlarni xohlaydi. Go'yo mehnatkash. Hozirgi narsalarning hech birini chalg'itmaslik kerak, chunki zavqlarni jiddiy qabul qilish kerak emas, ular muammoga olib keladi, biz hech qachon to'g'ri yo'lni topa olmaymiz, chunki biz ko'pincha buzuq! Go‘yo mehnat bilan band bo‘lgan narsalarning rohati biz uchun tutilgandek va hech kim bizni hech kim yetaklamaganligi sababli, ishning xatosi va azobi va zavqini hech kim ko‘ra olmaydi. donolik bilan, xushomadgo'ylikka loyiqroq zavqlarni qoldirib. Bu erda ko'p zavqlarning vazifalarini ham, baxtli vaqtlarni ham yomon ko'radiganlar. Biz ularni eng munosiblarning buyuk qabulini tark etishga olib boramiz, shuning uchun biz uchun donoroq narsani yomon ko'rgan har bir kishi, u hamma narsani boshqa yo'llar bilan xohlaydi. Bundan tashqari, biz tanlovni ayblaymiz, chunki u parvozdan qochganida ularni ochiq va og'riqli ko'tarishga intiladi, haqiqatan ham u mehnat tomonidan o'z zimmasiga olish uchun oson qarzlarga ergashishni xohlaydi. Qanday qiyin parvoz! Og'riqni ochish uchun xushomad so'zlari nafrat bilan tutilganlarning intilishlaridan zavqlanish yanada uzoqroqdek tuyuladi! U zavq-shavq hohlovchilarni rohat olishi uchun aqlningboshqa nafratlaridan qochadi, uning dardidir musibatdan Men ularni to'g'riga tushuntiraman ular yiqiladilar oson tug'ilishni ayblaymiz biz bu yerda oqsoqollar! Ular uni ochishadi. Og'riqlarni biroz donoroq kechiring, chunki ular bizdan ko'ra ko'proq buzilmagan xatolarni ta'minlaydi. Ularni qo'lga kiritish zarurati tufayli, ularni haqli ravishda kamroq qoldiradilar. Bu mehnat erkinlarning zimmasiga tushadi va ularga qanday erishishni hech kim bilmaydi. Ular ko'r-ko'rona ketishadi, bu sodir bo'ladi, ular rad etilishi yoki rad etilishi uchun lazzatlanishni ta'minlaydilar, lekin ular vazifalarni muvaffaqiyatli bajaradilar; Yuqorida aytib o'tilgan rohat-farog'atlarni o'zlari bilmaydigan haqli tanlash, o'zlariga chidashni istaydi va xuddi shu tarzda har qanday qattiqroq zavq-shavq ixtirochisini tanbeh qiladi, men bu orqali har qanday odam qanday buyukroq parvoz va tarbiya tug'ilganini tushuntiraman. bilan. Agar ular biz bu erda yumshoqlik sifatida ko'rsatishimiz mumkin bo'lgan og'riq va kamroq og'riqdan voz kechmasalar, donolar bilan aqlning zavqidan qoching. Hech bo'lmaganda, hozirgi azob-uqubatlar va azoblardan zavq qochib qutulganlar. Vaqtlar hech narsa emas va aqlning vaqti? Ammo ba'zilar zavqlanish uchun, shuning uchun ular qayerdan muvaffaqiyat qozonishsa, ular qanday maqsadga erishishni xohlashlaridan qat'i nazar, biz ularni ko'zbo'yamachilikda ayblaymiz, natija ko'pincha ochiladi, go'yo tanlovga ergashish uchun cho'l farqi. og'riqlar buzuq yoki yoqimli. Hamma narsa albatta sodir bo'ladi! Va shunday bo'ladiki, ayblovchilarning nafratlari ishni yanada baxtiyorlik bilan ta'minlaganlarida hech kim kam bo'lmagan so'zlarni tark etadi, lekin tanlagan narsa Ammo hozirgi paytda har qanday oddiy odam sifatida oqilona tanlash kerakmi? Ofislardan katta muammoni olib, unga g'amxo'rlik qilish oqilonami? Go'yo, bu qiyin paytlarda, bu vaqtda ba'zi erkalashlar uchun hech narsa bo'lmagan farqdagi har qanday og'riq rohat bo'lib, undan qochadi yoki rohat-farog'atga ochiq bo'lish uchun tug'ilgani uchun aybdor emasmi? Nahotki maqtovchilarning oqsoqollari? Shuningdek, biz uni katta lavozimlarni tanlashning oqibati bilan ayblamaymiz, lekin bundan tananing o'zini rad etishdan qutulishning iloji yo'qligi sababli, vaqti-vaqti bilan ma'lum vaqtlarda biror narsaga to'sqinlik qiladimi? Baxtli istaklar davrida, qarzga bo'lgan zavqni qayerdan rad qilishimiz kerak? U ayblovchilarning mehnati tufayli qabul qilinishga intilsin. Chunki og'riq bilan, kimdir bu kechirimni zavq uchun ayblaydi, agar u narsalarning so'zlarini xohlamasa, lekin ularni hayot haqiqatining ba'zi vazifalariga yumshatishni yomon ko'radi. U og'riqlar bilan ko'r bo'lib qoladi, haqiqatan ham, uning tanlovi eng munosib bo'lganlardan tug'ilganlarga qaraganda qattiqroqdir, chunki ular muammo me'morining zavqi bilan kamroq buziladi. Men buzuqlarning eng munosiblarining muammolarini qabul qilaman, lekin shuni tushuntiramanki, biz undan keyin keladigan xushomadgo'ylikni, go'yo ular katta oqibatlarga olib keladigandek qabul qilishimiz kerak va biz ularni hayotda ko'p va kam qilishimiz mumkin. Mehnat natijasida, biz tanlay olmaydigan hech kimni rad etish orqali hozirgi kunning tug'ilmagan zavqlari ochiladi, eng kichik to'siqlarning barchasi kattaroq og'riqlar uchun ayblanadi! Ba'zida me'mor tug'ilgan hayot qarzlari tufayli yiqilib, kechirimga ergashadi va keyin nima bo'ladi? Va aynan shu narsaga ergashish kerak";
+let massiv = text.split([" "]),
+  input = document.querySelector("input"),
+  text2 = document.querySelector("#text"),
+  achko = document.querySelector(".achko"),
+  qayta = document.querySelector(".qayta"),
+  boshla = document.querySelector(".boshla"),
+  min = document.querySelector(".min"),
+  sec = document.querySelector(".sec"),
+  // selectItem = document.querySelector('select'),
+  umumiy = 180,
+  daqiqa = parseInt(umumiy / 60),
+  soniya = umumiy % 60,
+  soni = 0,
+  random = null,
+  timer = [1, 2, 3, 4],
+  timeItems = document.querySelector('.timeItems');
+input.disabled = true;
+
+
+function vaqtInnerFunc() {
+  min.innerHTML = daqiqa > 9 ? daqiqa : "0" + daqiqa;
+  sec.innerHTML = soniya > 9 ? soniya : "0" + soniya;
+}
+function vaqtRun() {
+  umumiy--;
+  daqiqa = parseInt(umumiy / 60);
+  soniya = umumiy % 60;
+}
+vaqtInnerFunc()
+let initialState = (son = 60) => {
+  umumiy = son;
+  daqiqa = parseInt(umumiy / 60);
+  soniya = umumiy % 60;
+  soni = 0;
+  random = null;
+  vaqtInnerFunc()
+}
+function tezYoz() {
+  vaqtRun()
+  vaqtInnerFunc();
+  let timeIs = setInterval(() => {
+    vaqtRun()
+    if (umumiy === 0) {
+      clearInterval(timeIs)
+      input.innerHTML = ''
+      input.disabled = true
+      qayta.innerHTML = "Qayta o'ynash"
+      qayta.style.display = "inline-block"
+    }
+    vaqtInnerFunc();
+  }, 1000);
+  function randomFunc() {
+    return random = Math.ceil(Math.random() * massiv.length - 1)
+  }
+  randomFunc();
+  function newRandom() {
+    randomFunc();
+    text2.innerHTML = massiv[random]
+  }
+  newRandom();
+  input.addEventListener("input", () => {
+    if (input.value === massiv[random]) {
+      soni++
+      achko.innerHTML = soni
+      input.value = ""
+      newRandom();
+    }
+  })
+}
+qayta.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (umumiy === 0) {
+    initialState(60)
+  }
+  tezYoz();
+  input.disabled = false
+  qayta.style.display = "none"
+  input.focus()
+})
+timer.map((item, index) => {
+  timeItems.innerHTML += `
+    <span class="timeItem" onclick=handleClick(${index}) > ${item} </span>
+  `
+})
+let timeItem = document.querySelectorAll('.timeItem')
+function removeStyle() {
+  timeItem.forEach((i) => {
+    i.style.cssText = `
+      background-color: #272727
+    `
+  })
+}
+function addStyle(son) {
+  timeItem[son].style.cssText = `
+      background-color:wheat;
+      color:black
+    `
+}
+addStyle(0)
+let handleClick = (param) => {
+  removeStyle()
+  addStyle(param)
+  setTime(timer[param])
+}
